ec161c17fc0977cf7ae238c82a11580e741bb48d
ekintkara <taylanekin01@gmail.com>
2022-08-26 19:40:09 +0300

    tutorial pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Tutorial from "./pages/Tutorial";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Tutorial />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Tutorial.js
+++ src/pages/Tutorial.js
@@ -1,9 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
 
 export default class Tutorial extends Component {
   render() {
     return (
-      <div>Tutorial</div>
-    )
+      <div className="tutorial-container">
+        <h1 className="tutorial-title"> WELCOME TO TİCTACTOE </h1>
+        <h2 className="tutorial-text">
+          is a paper-and-pencil game for two players who take turns marking the
+          spaces in a three-by-three grid with X or O. The player who succeeds
+          in placing three of their marks in a horizontal, vertical, or diagonal
+          row is the winner. It is a solved game, with a forced draw assuming
+          best play from both players.
+        </h2>
+        <Link className="tutorial-button" to={"/game"}>
+          START
+        </Link>
+      </div>
+    );
   }
 }
